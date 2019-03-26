38bb7191e1585a3f97e6b63c10b569fe9733bbf0
GabeLeeData <gabe95sk@gmail.com>
2019-03-26 16:51:33 -0400

    Implemented Camera Functionality

--- app/src/main/java/gabriellee/project/criminalintent/modelLayer/CrimeLab.java
+++ app/src/main/java/gabriellee/project/criminalintent/modelLayer/CrimeLab.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -80,6 +81,11 @@ public class CrimeLab {
         values.put(CrimeTable.Cols.SUSPECT, crime.getSuspect());
 
         return values;
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, crime.getPhotoFilename());
     }
 
     public void updateCrime(Crime crime) {
